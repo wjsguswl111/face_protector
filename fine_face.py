d0bcbcca3b9c6dd7a5901a97a1286fa9792e4f23
DESKTOP-29I7ERL\user <eor84@naver.com>
2021-10-19 17:02:35 +0900

    얼굴검출 수정1

--- fine_face.py
+++ fine_face.py
@@ -1,26 +1,31 @@
 import cv2
 
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_classifier1 = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+face_classifier2 = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+face_classifier3 = cv2.CascadeClassifier('haarcascade_frontalface_alt_tree.xml')
+face_classifier4 = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
-cam = cv2.VideoCapture("vfile.mp4")
+cam = cv2.VideoCapture("dark.mp4")
 
 a = 1
 
-
 cv2.namedWindow('Face')
-
-
 
 while True:
     img, frame = cam.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_classifier.detectMultiScale(gray,1.1,3)
-
+    faces = face_classifier.detectMultiScale(gray, 1.1, 1)
+    
+    #cv2.CascadeClassifier.detectMultiScale(image[, scaleFactor[, minNeighbors[, flags[, minSize[, maxSize]]]]])
+    
     for (x,y,w,h) in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+
+    #faces4 = face_classifier4.detectMultiScale(gray, 1.0, 3)
+    #for (x,y,w,h) in faces:
+    #    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),2)
 
     frame = cv2.resize(frame,(int(frame.shape[1]*a), int(frame.shape[0]*a)))
 
