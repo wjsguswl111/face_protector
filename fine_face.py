4050278068a2d5ae39db166ec1c916c0713cd72d
DESKTOP-29I7ERL\user <eor84@naver.com>
2021-10-25 00:56:09 +0900

    얼굴검출 수정(imutils)

--- fine_face.py
+++ fine_face.py
@@ -1,33 +1,41 @@
+import numpy as np
 import cv2
+import os
+import imutils
 
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-face_classifier1 = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-face_classifier2 = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-face_classifier3 = cv2.CascadeClassifier('haarcascade_frontalface_alt_tree.xml')
-face_classifier4 = cv2.CascadeClassifier('haarcascade_profileface.xml')
+protoPath = "deploy.prototxt"
+modelPath = "res10_300x300_ssd_iter_140000.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-cam = cv2.VideoCapture("dark.mp4")
-
-a = 1
-
-cv2.namedWindow('Face')
+cam = cv2.VideoCapture("see.mp4")
 
 while True:
     img, frame = cam.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = imutils.resize(frame, width=600)
+    (h, w) = frame.shape[:2]
 
-    faces = face_classifier.detectMultiScale(gray, 1.1, 1)
+    imageBlob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=False, crop=False)
     
-    #cv2.CascadeClassifier.detectMultiScale(image[, scaleFactor[, minNeighbors[, flags[, minSize[, maxSize]]]]])
-    
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),2)
+    detector.setInput(imageBlob)
+    detections = detector.forward()
 
-    #faces4 = face_classifier4.detectMultiScale(gray, 1.0, 3)
-    #for (x,y,w,h) in faces:
-    #    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),2)
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
 
-    frame = cv2.resize(frame,(int(frame.shape[1]*a), int(frame.shape[0]*a)))
+        if confidence > 0.5 :
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            face = frame[startY:endY, startX:endX]
+            (fH, fW) = face.shape[:2]
+
+            if fW < 20 or fH < 20 :
+                continue
+
+            faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96), (0, 0, 0), swapRB = True, crop = False)
+
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
 
     cv2.imshow('Face',frame)
     if cv2.waitKey(1)==27:
