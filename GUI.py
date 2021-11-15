80f5ca92364a4b1152c3556e0212d40e587ab45a
wjsguswl111 <wjsguswl7143@naver.com>
2021-11-16 00:12:15 +0900

    Merge Chae

--- GUI.py
+++ GUI.py
@@ -1,64 +1,75 @@
-import cv2
-import os
-
-import tkinter as tk
-from tkinter import filedialog
+from tkinter import *
 from PIL import ImageTk, Image
+import cv2
+from tkinter import filedialog
+import os
+import sys
 
+window = Tk()
 
-root = tk.Tk()
-root.title("face_protector")
-root.geometry("1200x700+200+50")
-root.resizable(True, True)
+window.title("Face Protector")
+window.geometry("1200x700+200+50")
+window.resizable(True, True)
 
-filename = filedialog.askopenfilename(initialdir="/", title="Select file",
-                                    filetypes=(("MP4 files", "*.mp4"),("AVI files","*.avi"),
-                                    ("all files", "*.*")))
+label = Label(window, text="동영상")
+label.grid(row=0, column=0)
 
+frame_video = Frame(window, bg="white", width=720, height=480)
+frame_video.grid(row=1,column=0)
 
-frm = tk.Frame(root, bg="white", width=720, height=480)
-frm.grid(row=1, column=0)
+label1 = Label(frame_video)
+label1.grid()
 
-lbl1 = tk.Label(frm)
-lbl1.grid()
-
-#os.chdir('D:/GEODATA') # 현재 작업 디렉토리 변경
-cap = cv2.VideoCapture(filename) # VideoCapture 객체 정의
-
-def video_play():
-    ret, frame = cap.read()
-    if not ret:
-        cap.release()
-        return
+def video_stream():
+    _, frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
     img = Image.fromarray(frame)
     imgtk = ImageTk.PhotoImage(image=img)
-    lbl1.imgtk=imgtk
-    lbl1.configure(image=imgtk)
-    lbl1.after(10, video_play)
+    label1.imgtk = imgtk
+    label1.configure(image=imgtk)
+    label1.after(10, video_stream) 
 
-# 작업 완료 후 해제
+def Load():
+    global cap
+    filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                          filetypes=(("MP4 files", "*.mp4"),("AVI files","*.avi"),
+                                          ("all files", "*.*")))
+    cap = cv2.VideoCapture(filename)
+    video_stream()
 
-'''cap = cv2.VideoCapture(filename)
-frameRate = 33
-
-while True: # 동영상 재생
-    ret, frame = cap.read()
-    if not(ret):
-        break
-
-    cv2.imshow('freme', frame)
-    key = cv2.waitKey(frameRate)
-
-    if key == 27:
-        break
-
-    if cap.isOpened():
-        cap.release()
-cv2.destroyAllWindows()'''
+def Save():
+    filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
+                                          filetypes=(("MP4 files", "*.mp4"),("AVI files","*.avi"),
+                                          ("all files", "*.*")))
+    print(filename)
 
 
-video_play()
-root.mainloop()
+
+
+def domenu():
+    print("OK")
+
+menubar = Menu(window)
+filemenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New", command=domenu)
+filemenu.add_command(label="Open", command=Load)
+filemenu.add_command(label="Save", command=Save)
+filemenu.add_command(label="Save as...", command=Save)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=window.quit)
+editmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Edit", menu=editmenu)
+editmenu.add_command(label="Copy", command=domenu)
+editmenu.add_command(label="Paste", command=domenu)
+editmenu.add_separator()
+editmenu.add_command(label="Delete", command=domenu)
+helpmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About...", command=domenu)
+
+window.config(menu=menubar)
+
+window.mainloop()
 
 
