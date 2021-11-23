67bfccdd1ba99b842b4f8fe71d3e30a23f72996e
wjsguswl111 <wjsguswl7143@naver.com>
2021-11-23 16:37:27 +0900

    gui 및 전신 코드 수정

--- GUI.py
+++ GUI.py
@@ -96,7 +96,6 @@ class CWidget(QMainWindow):
         thirdLayout.addWidget(self.pauseButton, 1)
         thirdLayout.addWidget(self.stopButton, 1)
         
-
         layout = QVBoxLayout()
         layout.addLayout(topLayout, 1)
         layout.addWidget(videoWidget, 1200)
